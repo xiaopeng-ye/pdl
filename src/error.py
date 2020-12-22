c77ee9f1a151830f2266e011ef67024a052e4c80
dsdiois <dsdiois98@gmail.com>
2020-12-22 20:12:50 +0100

    a

--- src/error.py
+++ src/error.py
@@ -4,7 +4,7 @@ class GestorError:
     def __init__(self):
         self._error_file = open('error.txt', 'w', encoding='UTF-8')
         self.error_lexico = {'_': 'No es válido empezar con _ en un identificador',
-                              '\'': 'Cadena de texto invalido',
+                              '\'': 'Cadena de texto inválido',
 
                               }
 
